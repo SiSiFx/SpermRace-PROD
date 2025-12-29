e5df05d911ea3b87b77d4f5bb756a3d8f462889f
SiSiFx <155984437+SiSiFx@users.noreply.github.com>
2025-12-29 19:21:18 +0000

    Fix Vercel: import shared constants from source

--- packages/client/src/NewGameView.tsx
+++ packages/client/src/NewGameView.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef } from 'react';
 import * as PIXI from 'pixi.js';
 import { useWs } from './WsProvider';
 import { HudManager } from './HudManager';
-import { WORLD as S_WORLD } from '../../shared/dist/constants.js';
+import { WORLD as S_WORLD } from '../../shared/src/constants.ts';
 
 interface Car {
   x: number;
